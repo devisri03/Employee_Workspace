1c89c8b0dc2b1106504f95105abb5969b81be801
siri1592 <devisri1592@gmail.com>
2022-04-08 09:49:58 +0530

    Initial commit

---
+++ EmployeeDepartmentApplication/src/test/java/com/ls/EmployeeDepartmentApplication/EmployeeDepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.EmployeeDepartmentApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDepartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
